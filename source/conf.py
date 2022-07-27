5d4c92657c991412dd952efbde170326f4c073ef
@boand <dongba@aimachip.com>
2022-07-27 19:35:10 +0800

    修改内容

--- source/conf.py
+++ source/conf.py
@@ -31,8 +31,8 @@ html_logo = './_static/logo.png'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-  'recommonmark',
-  'sphinx_markdown_tables'
+  'recommonmark'
+  # 'sphinx_markdown_tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
