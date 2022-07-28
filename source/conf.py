67d8f97021ca4fb9571b0f0ec08bc536915b4292
@boand <dongba@aimachip.com>
2022-07-28 10:45:14 +0800

    修改conf.py

--- source/conf.py
+++ source/conf.py
@@ -68,3 +68,10 @@ html_static_path = ['_static']
 html_show_sourcelink = False
 
 master_doc = 'index'
+
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
